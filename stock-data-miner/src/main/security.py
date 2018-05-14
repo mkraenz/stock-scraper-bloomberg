48a464f2312334385232f6621ae0e518e30afdfb
Mirco Kraenz <prosingularity@yahoo.de>
2018-05-15 02:21:46 +0900

    #1 ADD class Security with PB, BP, BM ratios and market cap

---
+++ stock-data-miner/src/main/security.py
@@ -0,0 +1,33 @@
+'''
+Created on 15.05.2018
+
+@author: Mirco
+'''
+import math
+
+class Security(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, name):
+        '''
+        Constructor
+        '''
+        self.name = name
+        self.price = math.inf
+        self.book = 0
+        self.shares_outstanding = 0
+    
+    def get_P_to_B(self):
+        return self.price / self.book
+    
+    def get_B_to_P(self):
+        return self.book / self.price
+    
+    def get_market_cap(self):
+        return self.shares_outstanding*self.price
+    
+    def get_B_to_M(self):
+        return self.book / self.get_market_cap()
