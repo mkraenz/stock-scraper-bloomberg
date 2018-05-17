55ee295f323640c9270b0f87408ef1f340668c3b
Mirco Kraenz <prosingularity@yahoo.de>
2018-05-17 14:29:28 +0900

    #8 ADD setup.py and file to start all tests

---
+++ test/tests.py
@@ -0,0 +1,7 @@
+from test_miner.test_security import TestSecurityDataMinerBloomberg, TestSecurity
+import unittest
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
