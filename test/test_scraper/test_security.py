9e946663c2f8d1c02ad13b46e017dbf1be0a81a0
Mirco Kraenz <prosingularity@yahoo.de>
2018-05-17 15:44:07 +0900

    #11 REFACTOR test data is instance datafield not class datafield

--- test/test_scraper/test_security.py
+++ test/test_scraper/test_security.py
@@ -30,13 +30,20 @@ class TestSecurity(unittest.TestCase):
         self.assertEqual(self.stock.get_B_to_M(), 0.5)
         
 class TestScraperBloomberg(unittest.TestCase):
+
+    def __init__(self, tests=()):
+        super().__init__(tests)
+        self.html_example = self.load_html_test_data()
     
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, 'bloomberg-eoan-2018-05-15.html')
-    file = open(filename, 'r')
-    html_example = file.read()
-    file.close()
-    
+
+    def load_html_test_data(self):
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'bloomberg-eoan-2018-05-15.html')
+        file = open(filename, 'r')
+        html_example = file.read()
+        file.close()
+        return html_example
+        
     def setUp(self):
         unittest.TestCase.setUp(self)
         self.miner = ScraperBloomberg('')
@@ -47,15 +54,15 @@ class TestScraperBloomberg(unittest.TestCase):
         self.assertEqual(2300000, self.miner._ScraperBloomberg__parse_human_readable_number_to_int('2.3M'))
 
     def test_mine_price(self):
-        self.miner.update_html_soup(TestScraperBloomberg.html_example)
+        self.miner.update_html_soup(self.html_example)
         self.assertEqual(self.miner.mine_price(), 9.29)
         
     def test_mine_price_to_book(self):
-        self.miner.update_html_soup(TestScraperBloomberg.html_example)
+        self.miner.update_html_soup(self.html_example)
         self.assertEqual(self.miner.mine_price_to_book(), 4.5789)
         
     def test_mine_shares_outstanding(self):
-        self.miner.update_html_soup(TestScraperBloomberg.html_example)
+        self.miner.update_html_soup(self.html_example)
         self.assertEqual(self.miner.mine_shares_outstanding(), 2.2 * 10**9)
 
 if __name__ == "__main__":
