36e4082a5587ff0c23d9922bc136e15db13b805b
Mirco Kraenz <prosingularity@yahoo.de>
2018-05-23 23:05:39 +0900

    #22 UPDATE remove scraper parameter from Manager
    - Manager creates Scrapers itself

--- src/scraper/builder.py
+++ src/scraper/builder.py
@@ -5,7 +5,6 @@ Created on 18.05.2018
 '''
 from scraper.importer_csv import ImporterCSV
 from scraper.exporter_csv import Exporter_CSV
-from scraper.scraper_bloomberg import ScraperBloomberg
 from scraper.manager import Manager
 from scraper.security_list import SecurityList
 
@@ -15,7 +14,6 @@ class Builder(object):
     def start(self):
         importer = ImporterCSV()
         exporter = Exporter_CSV()
-        scraper = ScraperBloomberg()
-        manager = Manager(importer, exporter, scraper, SecurityList())
+        manager = Manager(importer, exporter, SecurityList())
         
         manager.manage()
--- src/scraper/manager.py
+++ src/scraper/manager.py
@@ -6,26 +6,32 @@ Created on 17.05.2018
 from scraper.security import Security
 from scraper.i_exporter import IExporter
 from scraper.i_importer import IImporter
-from scraper.i_scraper import IScraper
+from scraper.scraper_bloomberg import ScraperBloomberg
 
 
 class Manager(object):
     
-    def __init__(self, importer, exporter, scraper, security_list):
+    def __init__(self, importer, exporter, security_list):
+        '''
+        :param importer: instance of IImporter
+        :param exporter: instance of IExporter
+        :param security_list:
+        '''
         assert isinstance(importer, IImporter)
         assert isinstance(exporter, IExporter)
-        assert isinstance(scraper, IScraper)
-        self.importer = importer 
+        self.importer = importer
         self.exporter = exporter
         self.security_list = security_list
-        self.scraper = scraper
-    
+
+    def fill_security_list(self, importer):
+        for pair in importer:
+            sec = Security(pair[0], pair[1], ScraperBloomberg())
+            self.security_list.append(sec)
+
     def manage(self):
         file_path_relative_to_importer = '../../test/test_scraper/test.csv'
         self.importer.load_file(file_path_relative_to_importer)
-        for pair in self.importer:
-            sec = Security(pair[0], pair[1], self.scraper)
-            self.security_list.append(sec)
+        self.fill_security_list(self.importer)
         self.security_list.update()
         
         self.importer.cleanup()
