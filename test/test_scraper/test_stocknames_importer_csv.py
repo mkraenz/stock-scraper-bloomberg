a9946d9c440498cad6d271edb46b899d0bb167f0
Mirco Kraenz <prosingularity@yahoo.de>
2018-05-24 16:46:13 +0900

    #28 RENAME test.csv to test_input.txt and Importer_CSV to Importer

--- test/test_scraper/test_stocknames_importer_csv.py
+++ test/test_scraper/test_stocknames_importer_csv.py
@@ -4,17 +4,17 @@ Created on 21.05.2018
 @author: Mirco
 '''
 import unittest
-from scraper.importer_csv import ImporterCSV
+from scraper.importer import Importer
 
 
 class TestStockNamesImporterCSV(unittest.TestCase):
 
     def test_iterator(self):
         symbols = ['GOOGL:US', 'FB:US']
-#         path_rel_to_project_root = 'test_scraper/test.csv' # for eclipse
-        path_rel_to_project_root = 'test/test_scraper/test.csv' # for travis and official setup
+#         path_rel_to_project_root = 'test_scraper/test_input.txt' # for eclipse
+        path_rel_to_project_root = 'test/test_scraper/test_input.txt' # for travis and official setup
         
-        importer = ImporterCSV(path_rel_to_project_root)
+        importer = Importer(path_rel_to_project_root)
         importer.load_file()
         for symbol in importer:
             self.assertIn(symbol, symbols)
